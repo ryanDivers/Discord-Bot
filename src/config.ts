6d7f2ca6cb71c8a3570bfea5550b9d327008b1ba
Ryan Divers <ryan.divers@googlemail.com>
2023-07-08 09:48:05 +0100

    add type checker for config to remove any type

--- src/config.ts
+++ src/config.ts
@@ -1,4 +1,12 @@
 import dotenv = require('dotenv');
 dotenv.config();
 
-export const { BOT_TOKEN, MESSAGE_PREFIX }: { BOT_TOKEN: string; MESSAGE_PREFIX: string } = process.env as any;
+const hasValue = (configValue: string | undefined) : string => {
+    if (!configValue) {
+        throw new Error('Config has to be defined');
+    }
+    return configValue;
+}
+
+export const BOT_TOKEN = hasValue(process.env.BOT_TOKEN);
+export const MESSAGE_PREFIX = hasValue(process.env.MESSAGE_PREFIX);

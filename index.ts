20d206aee1ff7f208cc2b5444d46429e3df50ad3
Ryan Divers <ryan.divers@googlemail.com>
2023-07-08 09:50:12 +0100

    export events from events file

--- index.ts
+++ index.ts
@@ -1,8 +1,9 @@
 import { Client } from 'discord.js';
 import { BOT_TOKEN } from './src/config';
+import events from './src/events';
 import { newMessageHandler } from './src/handlers';
 
 const client = new Client({ intents: ["Guilds", "GuildMessages", "MessageContent"]});
 client.login(BOT_TOKEN);
 
-client.on('messageCreate', newMessageHandler);
+client.on(events.newMessage, newMessageHandler);

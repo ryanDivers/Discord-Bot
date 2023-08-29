f2da0c1e1d15dbcb62a95c08077e55853f159b0a
Ryan Divers <ryan.divers@googlemail.com>
2023-08-29 19:47:20 +0100

    feat: add list of help commands

--- src/handlers/commandHandlers/help.ts
+++ src/handlers/commandHandlers/help.ts
@@ -1,9 +1,20 @@
 import { Message } from 'discord.js';
 import { Logger } from 'pino';
 
+const helpMessage = `
+    !ping       - ping command
+    !play <url> - play/add track to play queue
+    !queue      - get current play queue
+    !clear      - clear current queue
+    !skip       - skip current track
+    !pause      - pause current track
+    !unpause    - unpause current track
+    !stop       - stop current track
+`;
+
 const help = (message: Message, logger: Logger): void => {
     logger.info({ msg: 'Handling Help Command' });
-    message.reply('Help is not configured yet');
+    message.reply(helpMessage);
 };
 
 export { help };
